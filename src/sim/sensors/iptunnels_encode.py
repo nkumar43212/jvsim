89fac21c1c40704d8a9e5f6ff26100725f1ce116
nitin <kumarn@juniper.net>
2016-04-13 17:37:46 -0700

    add tunnel type string in the iptunnels mock sensor

--- src/sim/sensors/iptunnels_encode.py
+++ src/sim/sensors/iptunnels_encode.py
@@ -4,7 +4,10 @@ import iptunnels_pb2
 def jv_encode_iptunnels(jnpr, index, lc_slot, system_params):
     iptunnels_stats = jnpr.Extensions[iptunnels_pb2.jnpr_iptunnels_statistics_ext]
     record    = iptunnels_stats.iptunnel_stats_records.add()
-    record.name = "100.100.100." + str(index) +  ":" + "200.200.200." + str(index);
+    tunnel_type = " gre";
+    if (random.randint(1, 3) == 1):
+        tunnel_type = " udp";
+    record.name = "src = 100.100.100." + str(index) +  " dst = 200.200.200." + str(index) + tunnel_type;
     record.packets            = random.randint(1, 7000000);
     record.bytes              =  random.randint(1, 7000000);
     record.rpf_failed_packets = random.randint(1, 10);
