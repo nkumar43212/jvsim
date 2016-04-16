532c72e53e60989a17c082d1b0a480062dcafbda
abbas <abbas@juniper.net>
2016-04-16 01:45:40 -0700

    80-column guideline

--- src/grpc/server/AgentConsolidatorSystem.hpp
+++ src/grpc/server/AgentConsolidatorSystem.hpp
@@ -9,87 +9,93 @@
 #ifndef AgentConsolidatorSystem_hpp
 #define AgentConsolidatorSystem_hpp
 
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include "AgentServerProtos.h"
 #include "AgentSystem.hpp"
+#include "AgentServerIdManager.hpp"
 
 // Consolidates requests into the JUNOS system. Multiple subscriptions can
 // request for the same data and streaming profiles. This interface ensures that
-// we do not create multiple subscriptions on the JUNOS system and reuse the sensors
-
+// we do not create multiple subscriptions on the JUNOS system
+// and reuse the sensors
 class AgentConsolidatorSystemHandle {
     Telemetry::Path _request;
     uint64_t    _refcount;
     
 public:
     // Accessors
-    void     setRef (uint64_t val)                  { _refcount = val;  }
-    uint64_t getRef ()                              { return _refcount; }
-    void     decRef ()                              { --_refcount;      }
-    void     incRef ()                              { ++_refcount;      }
-    void     setRequest(const Telemetry::Path *request) { _request.CopyFrom(*request); }
-    const Telemetry::Path *getRequest ()                { return &_request; }
-    
+    void     setRef (uint64_t val)      { _refcount = val;  }
+    uint64_t getRef (void)              { return _refcount; }
+    void     decRef (void)              { --_refcount;      }
+    void     incRef (void)              { ++_refcount;      }
+    void     setRequest(const Telemetry::Path *request)
+                                        { _request.CopyFrom(*request); }
+    const Telemetry::Path *getRequest (void)
+                                        { return &_request; }
+
+    ~AgentConsolidatorSystemHandle() {}
+
     // Object lifecycle
-    static std::shared_ptr<AgentConsolidatorSystemHandle> create(AgentSystem *sys_handle,
-                                                                 const Telemetry::Path *request_path);
-    
+    static std::shared_ptr<AgentConsolidatorSystemHandle>
+    create(AgentSystem *sys_handle, const Telemetry::Path *request_path);
 
     // Free up a system handle
-    bool         destroy(AgentSystem *sys_handle);
-    ~AgentConsolidatorSystemHandle() {}
-    
+    bool destroy(AgentSystem *sys_handle);
+
     // Get the contents of the handle from the system. Used by the unit test infra
     Telemetry::Path *get(AgentSystem *sys_handle);
 
     // Class interface
-    static std::shared_ptr<AgentConsolidatorSystemHandle> find(const Telemetry::Path *request_path,
-                                                               bool *collision);
+    static std::shared_ptr<AgentConsolidatorSystemHandle>
+    find(const Telemetry::Path *request_path, bool *collision);
+
     static void insert(AgentSystem *sys_handle,
                        const Telemetry::Path *request_path,
                        std::shared_ptr<AgentConsolidatorSystemHandle> syshandle);
     static void remove(AgentSystem *sys_handle,
                        const Telemetry::Path *request_path);
-    static uint32_t getCount();
-    void description();
+    static uint32_t getCount(void);
+    void description(void);
 };
 
-typedef std::shared_ptr<AgentConsolidatorSystemHandle> AgentConsolidatorSystemHandlePtr;
+
+// Consolidator Handle
+typedef std::shared_ptr<AgentConsolidatorSystemHandle>
+                                            AgentConsolidatorSystemHandlePtr;
 
 class AgentConsolidatorHandle {
-    // Request Identifier
+    // Subscription Identifier
     std::string _request_id;
-    
+
     // List of system handles allocated
-    std::vector<AgentConsolidatorSystemHandlePtr> _syshandles;
-    
+    std::vector<AgentConsolidatorSystemHandlePtr> _consolidatorsyshandles;
+
 public:
     AgentConsolidatorHandle (const std::string request_id) : _request_id(request_id) {}
     ~AgentConsolidatorHandle ()
     {
-        _syshandles.clear();
+        _consolidatorsyshandles.clear();
     }
-    
+
     const std::string getId ()
     {
         return _request_id;
     }
-    
-    uint32_t getHandleCount ()
+
+    uint32_t getHandleCount (void)
     {
-        return (uint32_t) _syshandles.size();
+        return (uint32_t) _consolidatorsyshandles.size();
     }
-    
+
     AgentConsolidatorSystemHandlePtr getHandle (uint32_t pos)
     {
-        return pos < getHandleCount() ? _syshandles[pos] : NULL;
+        return pos < getHandleCount() ? _consolidatorsyshandles[pos] : NULL;
     }
-    
-    void addHandle (AgentConsolidatorSystemHandlePtr sys_handle)
+
+    void addHandle (AgentConsolidatorSystemHandlePtr consolidatorsyshandles)
     {
-        _syshandles.push_back(sys_handle);
+        _consolidatorsyshandles.push_back(consolidatorsyshandles);
     }
 };
 
