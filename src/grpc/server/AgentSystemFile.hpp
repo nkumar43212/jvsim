ab7e350e99ed30575de3b4ebcea75e8885667c85
Abbas Sakarwala <sakarwala77@gmail.com>
2016-04-26 11:35:11 -0700

    remove comments

--- src/grpc/server/AgentSystemFile.hpp
+++ src/grpc/server/AgentSystemFile.hpp
@@ -19,13 +19,8 @@ public:
     AgentSystemFile (AgentServerLog *logger, std::string file_name) :
                                 AgentSystem(logger), _outputFile(file_name) {}
 
-    // Add into the system
     void systemAdd(SystemId id, const Telemetry::Path *request_path);
-
-    // Remove from the system
     void systemRemove(SystemId id, const Telemetry::Path *request_path);
-
-    // Query the system
     Telemetry::Path * systemGet(SystemId sys_id);
 };
 
