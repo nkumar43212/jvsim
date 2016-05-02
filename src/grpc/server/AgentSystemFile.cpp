3e9ebd2abf5daa9d45afafbc21371bf2c4c88a71
Abbas Sakarwala <sakarwala77@gmail.com>
2016-05-02 16:16:51 -0700

    Test config in file

--- src/grpc/server/AgentSystemFile.cpp
+++ src/grpc/server/AgentSystemFile.cpp
@@ -8,6 +8,7 @@
 
 #include "AgentSystemFile.hpp"
 #include "OCTelemetryJsonGenerator.hpp"
+#include "JunosTelemetryJsonGenerator.hpp"
 
 void
 AgentSystemFile::systemAdd (SystemId id, const Telemetry::Path *request_path)
@@ -15,9 +16,18 @@ AgentSystemFile::systemAdd (SystemId id, const Telemetry::Path *request_path)
     // The common interface
     AgentSystem::systemAdd(id, request_path);
 
+#if defined(__OC_Telemetry_Config__)
+    bool mqtt = true;
     // Generate the Jsonized OC configuration
-    std::string config = OCTelemetryJsonGenerator::generate_json_oc_config(true,
+    std::string config = OCTelemetryJsonGenerator::generate_json_oc_config(mqtt,
                                     (id_idx_t)id.getId(), request_path);
+#else
+    // Generate the Jsonized Junos configuration
+    bool add = true;
+    bool mqtt = true;
+    std::string config = JunosTelemetryJsonGenerator::generate_json_junos_config
+                         (add, mqtt, (id_idx_t)id.getId(), request_path);
+#endif
 
     // Write it out
     _outputFile << "Push config (" << id.getId() << "):" << std::endl;
@@ -31,9 +41,18 @@ AgentSystemFile::systemRemove (SystemId id, const Telemetry::Path *request_path)
     // The common interface
     AgentSystem::systemRemove(id, request_path);
 
+#if defined(__OC_Telemetry_Config__)
+    bool mqtt = true;
     // Generate the Jsonized OC configuration
-    std::string config = OCTelemetryJsonGenerator::generate_json_oc_config(true,
+    std::string config = OCTelemetryJsonGenerator::generate_json_oc_config(mqtt,
                                     (id_idx_t)id.getId(), request_path);
+#else
+    // Generate the Jsonized Junos configuration
+    bool add = false;
+    bool mqtt = true;
+    std::string config = JunosTelemetryJsonGenerator::generate_json_junos_config
+                         (add, mqtt, (id_idx_t)id.getId(), request_path);
+#endif
 
     // Write it out
     _outputFile << "Remove config (" << id.getId() << "):" << std::endl;
