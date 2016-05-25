3d221b4bb63d5a2cbc631a9325d9f21006cad5b3
Abbas Sakarwala <sakarwala77@gmail.com>
2016-05-25 03:04:07 -0700

    PathValidator util

---
+++ src/grpc/server/PathValidator.cpp
@@ -0,0 +1,202 @@
+//
+//  PathValidator.cpp
+//  Test_programs
+//
+//  Created by Abbas Sakarwala on 5/24/16.
+//
+//
+
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <sstream>
+#include <cmath>
+#include "PathValidator.hpp"
+
+// PathInformation store
+PathInformationDB path_information_db;
+
+void
+PathValidator::_read_path_information_file (std::string filename,
+                                           Json::Value &path_information_json)
+{
+    // Open file and read the file
+    std::ifstream file(filename);
+    std::stringstream path_information_buffer;
+    path_information_buffer << file.rdbuf();
+    std::string path_information_str = path_information_buffer.str();
+    // std::cout << path_information_str << std::endl;
+
+    Json::StyledWriter styledWriter;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse(path_information_str,
+                                          path_information_json);
+    if (parsingSuccessful) {
+        _logger->log("Json file \"" + filename + "\" parsed successfully");
+        // _logger->log("JSONized output:");
+        // _logger->log(styledWriter.write(path_information_json));
+    } else {
+        _logger->log("Json file \"" + filename + "\" parsing error");
+        exit(0);
+    }
+}
+
+uint32_t
+PathValidator::_roundToThousandths (uint32_t num, uint32_t max)
+{
+    num += 500;
+    num /= 1000;
+    num = num * 1000;
+    num = (num > max) ? max: num;
+    return num;
+}
+
+void
+PathValidator::build_path_information_db (std::string filename)
+{
+    Json::Value path_information_json;
+
+    // Read from file and conver to json
+    _read_path_information_file(filename, path_information_json);
+
+    // Look for valid_paths
+    path_information_json = path_information_json["valid_paths"];
+    if (path_information_json == Json::nullValue) {
+        _logger->log("\'valid_paths\' list keyword missing");
+        exit(0);
+    }
+
+    // Extract from array
+    const Json::Value default_value = Json::nullValue;
+    if (path_information_json.isArray()) {
+        // Size of array = ?
+        int size = path_information_json.size();
+        _logger->log("Number of paths provided in json file = " +
+                     std::to_string(size));
+
+        for (int i = 0; i < size; i++) {
+            Json::Value obj = path_information_json.get(i, default_value);
+            Json::StyledWriter styledWriter;
+            _logger->log("Path " + std::to_string(i) +
+                         " -->" + styledWriter.write(obj));
+
+            // First check if "path" is present
+            if (obj["path"] == Json::nullValue) {
+                _logger->log("Path " + std::to_string(i) +
+                             " --> Missing \'path\' keyword");
+                // continue;
+                exit(0);
+            } else {
+                PathInformationPtr pPathInformation =
+                    std::make_shared<PathInformation>(obj["path"].asString());
+
+                if (obj["filter_support"] != Json::nullValue) {
+                    pPathInformation->filter_support =
+                                obj["filter_support"].asBool();
+                }
+
+                if (obj["suppress_unchanged_support"] != Json::nullValue) {
+                    pPathInformation->suppress_unchanged_support =
+                                obj["suppress_unchanged_support"].asBool();
+                }
+
+                if (obj["max_silent_interval_support"] != Json::nullValue) {
+                    pPathInformation->max_silent_interval_support =
+                                obj["max_silent_interval_support"].asBool();
+                }
+
+                if (obj["sample_frequency"]["min"] != Json::nullValue) {
+                    pPathInformation->sample_frequency_min =
+                                obj["sample_frequency"]["min"].asInt();
+                }
+
+                if (obj["sample_frequency"]["max"] != Json::nullValue) {
+                    pPathInformation->sample_frequency_max =
+                                obj["sample_frequency"]["max"].asInt();
+                }
+
+                // Now add in the db store
+                path_information_db[pPathInformation->path] = pPathInformation;
+            }
+        }
+    } else {
+        _logger->log("Invalid json format");
+        exit(0);
+    }
+}
+
+void
+PathValidator::print_path_information_db (void)
+{
+    // Iterate
+    for (PathInformationDB::iterator itr = path_information_db.begin();
+         itr != path_information_db.end();
+         itr++) {
+        std::string pathinfo_str = "<PathInformation DB>" + itr->first + "--->";
+        PathInformationPtr pPathInfo = itr->second;
+        pathinfo_str.append("path:" + pPathInfo->path + ",");
+        pathinfo_str.append("filter_support:" +
+                std::to_string(pPathInfo->filter_support) + ",");
+        pathinfo_str.append("suppress_unchanged_support:" +
+                std::to_string(pPathInfo->suppress_unchanged_support) + ",");
+        pathinfo_str.append("max_silent_interval_support:" +
+                std::to_string(pPathInfo->max_silent_interval_support) + ",");
+        pathinfo_str.append("sample_frequency_min:" +
+                std::to_string(pPathInfo->sample_frequency_min) + ",");
+        pathinfo_str.append("sample_frequency_max:" +
+                std::to_string(pPathInfo->sample_frequency_max));
+        _logger->log(pathinfo_str);
+    }
+}
+
+void
+PathValidator::delete_path_information_db (void)
+{
+    // Iterate
+    _logger->log("Erase the path information db");
+
+    PathInformationDB::iterator itr = path_information_db.begin();
+    while (itr != path_information_db.end()) {
+        itr = path_information_db.erase(itr);
+    }
+}
+
+void
+PathValidator::validate_path (Telemetry::Path &path)
+{
+    // Search the db
+    PathInformationPtr pPathInformation = path_information_db[path.path()];
+    if (pPathInformation == NULL) {
+        path.Clear();
+        return;
+    }
+
+    // Check filter
+    if (!pPathInformation->filter_support) {
+        path.clear_filter();
+    }
+
+    // Check suppress_unchanged
+    if (!pPathInformation->suppress_unchanged_support) {
+        path.clear_suppress_unchanged();
+    }
+
+    // Check max_silent_interval
+    if (!pPathInformation->max_silent_interval_support) {
+        path.clear_max_silent_interval();
+    }
+
+    // Check sample_frequency
+    uint32_t sample_frequency = path.sample_frequency();
+    uint32_t sample_frequency_min = pPathInformation->sample_frequency_min;
+    uint32_t sample_frequency_max = pPathInformation->sample_frequency_max;
+    if (sample_frequency < sample_frequency_min) {
+        path.set_sample_frequency(sample_frequency_min);
+    } else if (sample_frequency > sample_frequency_max) {
+        path.set_sample_frequency(sample_frequency_max);
+    } else {
+        uint32_t rounded = _roundToThousandths(sample_frequency,
+                                               sample_frequency_max);
+        path.set_sample_frequency(rounded);
+    }
+}
---
+++ src/grpc/server/PathValidator.hpp
@@ -0,0 +1,69 @@
+//
+//  PathValidator.hpp
+//  Telemetry Agent
+//
+//  Created: 5/24/16.
+//
+//  Authors: ABBAS SAKARWALA
+//
+//  Copyright © 2016 Juniper Networks. All rights reserved.
+//
+
+#ifndef PathValidator_hpp
+#define PathValidator_hpp
+
+#include <string>
+#include "agent.pb.h"
+#include "json/json.h"
+#include "AgentServerLog.hpp"
+
+#define SAMPLE_FREQUENCY_MIN                2000
+#define SAMPLE_FREQUENCY_MAX             3600000
+
+class PathInformation {
+public:
+    std::string path;
+    bool filter_support;
+    bool suppress_unchanged_support;
+    bool max_silent_interval_support;
+    uint32_t sample_frequency_min;
+    uint32_t sample_frequency_max;
+
+    PathInformation (std::string path) : path(path)
+    {
+        filter_support = true;
+        suppress_unchanged_support = false;
+        max_silent_interval_support = false;
+        sample_frequency_min = SAMPLE_FREQUENCY_MIN;
+        sample_frequency_max = SAMPLE_FREQUENCY_MAX;
+    }
+
+    ~PathInformation () {}
+};
+
+// PathInformation store
+typedef std::shared_ptr<PathInformation> PathInformationPtr;
+typedef std::map<std::string, PathInformationPtr> PathInformationDB;
+
+class PathValidator {
+    AgentServerLog *_logger;
+    void _read_path_information_file(std::string filename,
+                                     Json::Value &path_information_json);
+    uint32_t _roundToThousandths(uint32_t num, uint32_t max);
+public:
+    PathInformationDB path_information_db;
+
+    PathValidator (AgentServerLog *logger) : _logger(logger) {};
+    
+    ~PathValidator ()
+    {
+        // Delete the store
+        delete_path_information_db();
+    }
+    void build_path_information_db(std::string filename);
+    void print_path_information_db(void);
+    void delete_path_information_db(void);
+    void validate_path(Telemetry::Path &path);
+};
+
+#endif /* PathValidator_hpp */
