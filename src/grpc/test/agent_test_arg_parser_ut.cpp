1c18275367fda458bac0b68614fdefe44381ebec
Abbas Sakarwala <sakarwala77@gmail.com>
2016-05-27 02:49:27 -0700

    Fix compiler warnings and multiple_args test case

--- src/grpc/test/agent_test_arg_parser_ut.cpp
+++ src/grpc/test/agent_test_arg_parser_ut.cpp
@@ -16,7 +16,7 @@
 // Command Lineparsing
 TEST(args_parser, no_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", NULL};
+    char *argv[] = {(char *)"test", NULL};
 
     opts.parseArgs(1, argv);
 
@@ -28,7 +28,7 @@ TEST(args_parser, no_args) {
 
 TEST(args_parser, log_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-l", "log_file", NULL};
+    char *argv[] = {(char *)"test", (char *)"-l", (char *)"log_file", NULL};
 
     opts.parseArgs(3, argv);
     EXPECT_STREQ("log_file", opts.getLogFile());
@@ -36,7 +36,7 @@ TEST(args_parser, log_args) {
 
 TEST(args_parser, null_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-n", NULL};
+    char *argv[] = {(char *)"test", (char *)"-n", NULL};
 
     opts.parseArgs(2, argv);
 
@@ -45,7 +45,7 @@ TEST(args_parser, null_args) {
 
 TEST(args_parser, file_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-f", "test_file", NULL};
+    char *argv[] = {(char *)"test", (char *)"-f", (char *)"test_file", NULL};
 
     opts.parseArgs(3, argv);
 
@@ -55,7 +55,7 @@ TEST(args_parser, file_args) {
 
 TEST(args_parser, file_args_no_file) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-f", NULL};
+    char *argv[] = {(char *)"test", (char *)"-f", NULL};
 
     bool status = opts.parseArgs(2, argv);
     EXPECT_EQ(false, status);
@@ -64,7 +64,7 @@ TEST(args_parser, file_args_no_file) {
 
 TEST(args_parser, proc_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-p", NULL};
+    char *argv[] = {(char *)"test", (char *)"-p", NULL};
 
     opts.parseArgs(2, argv);
 
@@ -75,7 +75,8 @@ TEST(args_parser, proc_args) {
 
 TEST(args_parser, ini_config_test) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-c", "ini_config_file", NULL};
+    char *argv[] = {(char *)"test", (char *)"-c", (char *)"ini_config_file",
+                    NULL};
 
     opts.parseArgs(3, argv);
 
@@ -84,7 +85,7 @@ TEST(args_parser, ini_config_test) {
 
 TEST(args_parser, invalid_arg) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-x", NULL};
+    char *argv[] = {(char *)"test", (char *)"-x", NULL};
 
     bool status = opts.parseArgs(2, argv);
 
@@ -93,7 +94,7 @@ TEST(args_parser, invalid_arg) {
 
 TEST(args_parser, invalid_arg_non_print) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-\xa0", NULL};
+    char *argv[] = {(char *)"test", (char *)"-\xa0", NULL};
 
     bool status = opts.parseArgs(2, argv);
 
@@ -102,15 +103,19 @@ TEST(args_parser, invalid_arg_non_print) {
 
 TEST(args_parser, usage) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-u", NULL};
+    char *argv[] = {(char *)"test", (char *)"-u", NULL};
 
     opts.parseArgs(2, argv);
 }
 
 TEST(args_parser, multiple_args) {
     AgentServerCmdOptions opts;
-    char *argv[] = {"test", "-u", NULL};
+    char *argv[] = {(char *)"test", (char *)"-p",
+                    (char *)"-l", (char *)"log_file", NULL};
 
-    opts.parseArgs(2, argv);
+    opts.parseArgs(4, argv);
+    EXPECT_EQ(false, opts.isSystemModeFile());
+    EXPECT_EQ(false, opts.isSystemModeNull());
+    EXPECT_EQ(true, opts.isSystemModeProc());
+    EXPECT_STREQ("log_file", opts.getLogFile());
 }
-
