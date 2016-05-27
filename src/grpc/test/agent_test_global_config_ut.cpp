e05f9f5d2f5b554ab3362f2e542a3d3802cf18db
Abbas Sakarwala <sakarwala77@gmail.com>
2016-05-27 04:03:49 -0700

    Add GlobalConfig Test cases

--- src/grpc/test/agent_test_global_config_ut.cpp
+++ src/grpc/test/agent_test_global_config_ut.cpp
@@ -1,9 +1,26 @@
 //
 //  agent_test_global_config_ut.cpp
-//  grpc
 //
-//  Created by Abbas Sakarwala on 5/27/16.
+//  Created: 5/27/16.
+//
+//  Authors: ABBAS SAKARWALA
+//
 //  Copyright © 2016 Juniper Networks. All rights reserved.
 //
 
+#include "gtest/gtest.h"
 #include "agent_test_global_config_ut.hpp"
+
+TEST_F(GlobalConfigTest, init)
+{
+    EXPECT_STREQ("0.0.0.0", global_config.grpc_server_ip.c_str());
+    EXPECT_STREQ(SYSTEM_MODE_PROC, global_config.system_mode.c_str());
+}
+
+TEST_F(GlobalConfigTest, parse)
+{
+    GlobalConfig::parse(global_config_ini, global_config);
+    EXPECT_STREQ(TOPIC_INTERNAL_SUB_ID, global_config.subscribe_topic_name.c_str());
+    EXPECT_STREQ(SYSTEM_MODE_FILE, global_config.system_mode.c_str());
+    EXPECT_STREQ("/tmp", global_config.grpc_log_file_path.c_str());
+}
--- src/grpc/test/agent_test_global_config_ut.hpp
+++ src/grpc/test/agent_test_global_config_ut.hpp
@@ -1,14 +1,44 @@
 //
 //  agent_test_global_config_ut.hpp
-//  grpc
 //
-//  Created by Abbas Sakarwala on 5/27/16.
+//  Created: 5/27/16.
+//
+//  Authors: ABBAS SAKARWALA
+//
 //  Copyright © 2016 Juniper Networks. All rights reserved.
 //
 
 #ifndef agent_test_global_config_ut_hpp
 #define agent_test_global_config_ut_hpp
 
-#include <stdio.h>
+#include "GlobalConfig.hpp"
+
+// This is a test class
+class GlobalConfigTest: public testing::Test {
+public:
+    std::string         root_path;
+    std::string         global_config_ini;
+
+    void SetUp (void)
+    {
+        std::string path_validator_log;
+        char *env_rp = std::getenv("ROOTPATH");
+        if (env_rp != NULL) {
+            // if ROOTPATH env variable is set, set default log path
+            root_path = (std::string)env_rp;
+            global_config_ini = (std::string)env_rp +"/config/agent_server.ini";
+        } else {
+            std::cerr << "Please setup ROOTPATH environment variable."
+            << std::endl;
+            exit(1);
+        }
+    }
+
+    void TearDown (void)
+    {
+    }
+
+    static void *create(void *);
+};
 
 #endif /* agent_test_global_config_ut_hpp */
