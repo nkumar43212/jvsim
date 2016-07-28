c3078b0ea0e979fb5dc66b0d02ae9650e456d357
Abbas Sakarwala <sakarwala77@gmail.com>
2016-07-28 15:22:51 -0700

    Optic translation fix

--- src/grpc/lib-oc/OpenConfigOptics.cpp
+++ src/grpc/lib-oc/OpenConfigOptics.cpp
@@ -26,145 +26,145 @@ OpenConfigOptics::iterate (JuniperNetworksSensors *handle, telemetry::OpenConfig
         // Prefix
         oc_set_prefix(datap, BASE_OC_PATH_OPTICS, element.if_name(), BASE_OC_PATH_OPTICS_ATTR);
 
-        if (diags.optics_type()) {
+        if (diags.has_optics_type()) {
             kv = datap->add_kv();
             kv->set_key("optics_type");
             kv->set_uint_value(diags.optics_type());
         }
 
-        if (diags.module_temp()) {
+        if (diags.has_module_temp()) {
             kv = datap->add_kv();
             kv->set_key("module_temp");
             kv->set_double_value(diags.module_temp());
         }
 
-        if (diags.module_temp_high_alarm_threshold()) {
+        if (diags.has_module_temp_high_alarm_threshold()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_high_alarm_threshold");
             kv->set_double_value(diags.module_temp_high_alarm_threshold());
         }
 
-        if (diags.module_temp_low_alarm_threshold()) {
+        if (diags.has_module_temp_low_alarm_threshold()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_low_alarm_threshold");
             kv->set_double_value(diags.module_temp_low_alarm_threshold());
         }
 
-        if (diags.module_temp_high_warning_threshold()) {
+        if (diags.has_module_temp_high_warning_threshold()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_high_warning_threshold");
             kv->set_double_value(diags.module_temp_high_warning_threshold());
         }
 
-        if (diags.module_temp_low_warning_threshold()) {
+        if (diags.has_module_temp_low_warning_threshold()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_low_warning_threshold");
             kv->set_double_value(diags.module_temp_low_warning_threshold());
         }
 
-        if (diags.laser_output_power_high_alarm_threshold_dbm()) {
+        if (diags.has_laser_output_power_high_alarm_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_output_power_high_alarm_threshold_dbm");
             kv->set_double_value(diags.laser_output_power_high_alarm_threshold_dbm());
         }
 
-        if (diags.laser_output_power_low_alarm_threshold_dbm()) {
+        if (diags.has_laser_output_power_low_alarm_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_output_power_low_alarm_threshold_dbm");
             kv->set_double_value(diags.laser_output_power_low_alarm_threshold_dbm());
         }
 
-        if (diags.laser_output_power_high_warning_threshold_dbm()) {
+        if (diags.has_laser_output_power_high_warning_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_output_power_high_warning_threshold_dbm");
             kv->set_double_value(diags.laser_output_power_high_warning_threshold_dbm());
         }
 
-        if (diags.laser_output_power_low_warning_threshold_dbm()) {
+        if (diags.has_laser_output_power_low_warning_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_output_power_low_warning_threshold_dbm");
             kv->set_double_value(diags.laser_output_power_low_warning_threshold_dbm());
         }
 
-        if (diags.laser_rx_power_high_alarm_threshold_dbm()) {
+        if (diags.has_laser_rx_power_high_alarm_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_rx_power_high_alarm_threshold_dbm");
             kv->set_double_value(diags.laser_rx_power_high_alarm_threshold_dbm());
         }
 
-        if (diags.laser_rx_power_low_alarm_threshold_dbm()) {
+        if (diags.has_laser_rx_power_low_alarm_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_rx_power_low_alarm_threshold_dbm");
             kv->set_double_value(diags.laser_rx_power_low_alarm_threshold_dbm());
         }
 
-        if (diags.laser_rx_power_high_warning_threshold_dbm()) {
+        if (diags.has_laser_rx_power_high_warning_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_rx_power_high_warning_threshold_dbm");
             kv->set_double_value(diags.laser_rx_power_high_warning_threshold_dbm());
         }
 
-        if (diags.laser_rx_power_low_warning_threshold_dbm()) {
+        if (diags.has_laser_rx_power_low_warning_threshold_dbm()) {
             kv = datap->add_kv();
             kv->set_key("laser_rx_power_low_warning_threshold_dbm");
             kv->set_double_value(diags.laser_rx_power_low_warning_threshold_dbm());
-		}
+        }
 
-        if (diags.laser_bias_current_high_alarm_threshold()) {
+        if (diags.has_laser_bias_current_high_alarm_threshold()) {
             kv = datap->add_kv();
             kv->set_key("laser_bias_current_high_alarm_threshold");
             kv->set_double_value(diags.laser_bias_current_high_alarm_threshold());
         }
 
-        if (diags.laser_bias_current_low_alarm_threshold()) {
+        if (diags.has_laser_bias_current_low_alarm_threshold()) {
             kv = datap->add_kv();
             kv->set_key("laser_bias_current_low_alarm_threshold");
             kv->set_double_value(diags.laser_bias_current_low_alarm_threshold());
         }
 
-        if (diags.laser_bias_current_high_warning_threshold()) {
+        if (diags.has_laser_bias_current_high_warning_threshold()) {
             kv = datap->add_kv();
             kv->set_key("laser_bias_current_high_warning_threshold");
             kv->set_double_value(diags.laser_bias_current_high_warning_threshold());
         }
 
-        if (diags.laser_bias_current_low_warning_threshold()) {
+        if (diags.has_laser_bias_current_low_warning_threshold()) {
             kv = datap->add_kv();
             kv->set_key("laser_bias_current_low_warning_threshold");
             kv->set_double_value(diags.laser_bias_current_low_warning_threshold());
         }
 
-        if (diags.module_temp_high_alarm()) {
+        if (diags.has_module_temp_high_alarm()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_high_alarm");
             kv->set_bool_value(diags.module_temp_high_alarm());
         }
 
-        if (diags.module_temp_low_alarm()) {
+        if (diags.has_module_temp_low_alarm()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_low_alarm");
             kv->set_bool_value(diags.module_temp_low_alarm());
         }
 
-        if (diags.module_temp_high_warning()) {
+        if (diags.has_module_temp_high_warning()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_high_warning");
             kv->set_bool_value(diags.module_temp_high_warning());
         }
 
-        if (diags.module_temp_low_warning()) {
+        if (diags.has_module_temp_low_warning()) {
             kv = datap->add_kv();
             kv->set_key("module_temp_low_warning");
             kv->set_bool_value(diags.module_temp_low_warning());
         }
 
-		for (int j = 0; j < diags.optics_lane_diag_stats_size(); j++) {
+        for (int j = 0; j < diags.optics_lane_diag_stats_size(); j++) {
             const OpticsDiagLaneStats &lanediags = diags.optics_lane_diag_stats(j);
 
             std::string lanediag("lanediags/");
             std::string lanenumber;
 
-            if (lanediags.lane_number()) {
+            if (lanediags.has_lane_number()) {
                 kv = datap->add_kv();
                 lanenumber = std::to_string(lanediags.lane_number());
                 kv->set_key(lanediag + lanenumber + "/lane_number");
@@ -173,115 +173,115 @@ OpenConfigOptics::iterate (JuniperNetworksSensors *handle, telemetry::OpenConfig
                 lanenumber = "unknown";
             }
 
-            if (lanediags.lane_laser_temperature()) {
+            if (lanediags.has_lane_laser_temperature()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_temperature");
                 kv->set_double_value(lanediags.lane_laser_temperature());
             }
 
-            if (lanediags.lane_laser_output_power_dbm()) {
+            if (lanediags.has_lane_laser_output_power_dbm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_output_power_dbm");
                 kv->set_double_value(lanediags.lane_laser_output_power_dbm());
             }
 
-            if (lanediags.lane_laser_receiver_power_dbm()) {
+            if (lanediags.has_lane_laser_receiver_power_dbm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_receiver_power_dbm");
                 kv->set_double_value(lanediags.lane_laser_receiver_power_dbm());
             }
 
-            if (lanediags.lane_laser_bias_current()) {
+            if (lanediags.has_lane_laser_bias_current()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_bias_current");
                 kv->set_double_value(lanediags.lane_laser_bias_current());
             }
 
-            if (lanediags.lane_laser_output_power_high_alarm()) {
+            if (lanediags.has_lane_laser_output_power_high_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_output_power_high_alarm");
                 kv->set_bool_value(lanediags.lane_laser_output_power_high_alarm());
             }
 
-            if (lanediags.lane_laser_output_power_low_alarm()) {
+            if (lanediags.has_lane_laser_output_power_low_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_output_power_low_alarm");
                 kv->set_bool_value(lanediags.lane_laser_output_power_low_alarm());
             }
 
-            if (lanediags.lane_laser_output_power_high_warning()) {
+            if (lanediags.has_lane_laser_output_power_high_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_output_power_high_warning");
-				kv->set_bool_value(lanediags.lane_laser_output_power_high_warning());
+                kv->set_bool_value(lanediags.lane_laser_output_power_high_warning());
             }
 
-            if (lanediags.lane_laser_output_power_low_warning()) {
+            if (lanediags.has_lane_laser_output_power_low_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_output_power_low_warning");
                 kv->set_bool_value(lanediags.lane_laser_output_power_low_warning());
             }
 
-            if (lanediags.lane_laser_receiver_power_high_alarm()) {
+            if (lanediags.has_lane_laser_receiver_power_high_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_receiver_power_high_alarm");
                 kv->set_bool_value(lanediags.lane_laser_receiver_power_high_alarm());
             }
 
-            if (lanediags.lane_laser_receiver_power_low_alarm()) {
+            if (lanediags.has_lane_laser_receiver_power_low_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_receiver_power_low_alarm");
                 kv->set_bool_value(lanediags.lane_laser_receiver_power_low_alarm());
             }
 
-            if (lanediags.lane_laser_receiver_power_high_warning()) {
+            if (lanediags.has_lane_laser_receiver_power_high_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_receiver_power_high_warning");
                 kv->set_bool_value(lanediags.lane_laser_receiver_power_high_warning());
             }
 
-            if (lanediags.lane_laser_receiver_power_low_warning()) {
+            if (lanediags.has_lane_laser_receiver_power_low_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_receiver_power_low_warning");
                 kv->set_bool_value(lanediags.lane_laser_receiver_power_low_warning());
             }
 
-            if (lanediags.lane_laser_bias_current_high_alarm()) {
+            if (lanediags.has_lane_laser_bias_current_high_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_bias_current_high_alarm");
                 kv->set_bool_value(lanediags.lane_laser_bias_current_high_alarm());
             }
 
-            if (lanediags.lane_laser_bias_current_low_alarm()) {
+            if (lanediags.has_lane_laser_bias_current_low_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_bias_current_low_alarm");
                 kv->set_bool_value(lanediags.lane_laser_bias_current_low_alarm());
             }
 
-            if (lanediags.lane_laser_bias_current_high_warning()) {
+            if (lanediags.has_lane_laser_bias_current_high_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_bias_current_high_warning");
                 kv->set_bool_value(lanediags.lane_laser_bias_current_high_warning());
-			}
+            }
 
-            if (lanediags.lane_laser_bias_current_low_warning()) {
+            if (lanediags.has_lane_laser_bias_current_low_warning()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_laser_bias_current_low_warning");
                 kv->set_bool_value(lanediags.lane_laser_bias_current_low_warning());
-			}
+            }
 
-            if (lanediags.lane_tx_loss_of_signal_alarm()) {
+            if (lanediags.has_lane_tx_loss_of_signal_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_tx_loss_of_signal_alarm");
                 kv->set_bool_value(lanediags.lane_tx_loss_of_signal_alarm());
             }
 
-            if (lanediags.lane_rx_loss_of_signal_alarm()) {
+            if (lanediags.has_lane_rx_loss_of_signal_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_rx_loss_of_signal_alarm");
                 kv->set_bool_value(lanediags.lane_rx_loss_of_signal_alarm());
             }
 
-            if (lanediags.lane_tx_laser_disabled_alarm()) {
+            if (lanediags.has_lane_tx_laser_disabled_alarm()) {
                 kv = datap->add_kv();
                 kv->set_key(lanediag + lanenumber + "/lane_tx_laser_disabled_alarm");
                 kv->set_bool_value(lanediags.lane_tx_laser_disabled_alarm());
@@ -289,3 +289,4 @@ OpenConfigOptics::iterate (JuniperNetworksSensors *handle, telemetry::OpenConfig
         }
     }
 }
+
